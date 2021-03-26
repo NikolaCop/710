faa0c8a7ff6d1203057603fbb371066aaf037092
Dallen Pyrah <dallenpyrah@gmail.com>
2021-03-26 16:34:13 -0600

    Styling for home page fixed

--- 710.client/src/components/navbar.vue
+++ 710.client/src/components/navbar.vue
@@ -1,81 +1,77 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid bg-dark">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center">
-        <img
-          alt="logo"
-          src="../assets/img/cw-logo.png"
-          height="45"
-        />
-      </div>
     </router-link>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarText"
-      aria-controls="navbarText"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon" />
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <router-link :to="{ name: 'Home' }" class="nav-link">
-            Home
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link :to="{ name: 'About' }" class="nav-link">
-            About
-          </router-link>
-        </li>
-      </ul>
-      <span class="navbar-text">
-        <button
-          class="btn btn-outline-primary text-uppercase"
-          @click="login"
-          v-if="!user.isAuthenticated"
-        >
-          Login
-        </button>
+    <div class="row margin-l-1 justify-content-around">
+      <div class="col-1 mt-1 card card-navbar-blue bg-primary">
+      </div>
+      <div class="col-1 mt-1 card card-navbar-white">
+      </div>
+      <div class="col-9 mt-1 card card-navbar-primary bg-primary">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-1 margin-left">
+        <h1 class="text-light text-center mt-1">
+          <button
+            class="navbar-toggler text-light"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <h2>710</h2>
+            <span class="navbar-toggler-icon" />
+          </button>
+        </h1>
+      </div>
+      <div class="collapse ml-2 mt-2 col-10 text-right navbar-collapse" id="navbarText">
+        <span class="navbar-text">
+          <button
+            class="btn btn-outline-primary text-uppercase"
+            @click="login"
+            v-if="!user.isAuthenticated"
+          >
+            Login
+          </button>
 
-        <div class="dropdown" v-else>
-          <div
-            class="dropdown-toggle"
-            @click="state.dropOpen = !state.dropOpen"
-          >
-            <img
-              :src="user.picture"
-              alt="user photo"
-              height="40"
-              class="rounded"
-            />
-            <span class="mx-3">{{ user.name }}</span>
-          </div>
-          <div
-            class="dropdown-menu p-0 list-group w-100"
-            :class="{ show: state.dropOpen }"
-            @click="state.dropOpen = false"
-          >
-            <router-link :to="{ name: 'Account' }">
-              <div class="list-group-item list-group-item-action hoverable">
-                Account
-              </div>
-            </router-link>
+          <div class="dropdown text-light" v-else>
             <div
-              class="list-group-item list-group-item-action hoverable"
-              @click="logout"
+              class="dropdown-toggle"
+              @click="state.dropOpen = !state.dropOpen"
             >
-              logout
+              <img
+                :src="user.picture"
+                alt="user photo"
+                height="40"
+                class="rounded"
+              />
+              <span class="mx-3 text-light">{{ user.name }}</span>
+            </div>
+            <div
+              class="dropdown-menu p-0 list-group w-100"
+              :class="{ show: state.dropOpen }"
+              @click="state.dropOpen = false"
+            >
+              <router-link :to="{ name: 'Account' }">
+                <div class="list-group-item bg-dark text-lightlist-group-item-action hoverable">
+                  Account
+                </div>
+              </router-link>
+              <div
+                class="list-group-item bg-dark text-light list-group-item-action hoverable"
+                @click="logout"
+              >
+                logout
+              </div>
             </div>
           </div>
-        </div>
-      </span>
+        </span>
+      </div>
     </div>
-  </nav>
+  </div>
 </template>
 
 <script>
@@ -123,5 +119,30 @@ a:hover {
 }
 .nav-item .nav-link.router-link-exact-active{
   color: var(--primary);
+}
+.card-navbar-blue{
+  width: 20px;
+  height: 15px;
+  border-radius: 5rem;
+}
+.card-navbar-white{
+  width: 20px;
+  height: 15px;
+  border-radius: 5rem;
+}
+.card-navbar-primary{
+  width: 250px;
+  height: 15px;
+  border-radius: 5rem;
+}
+@media (min-width: 576px) {
+  .margin-left{
+  margin-left: 1rem;
+  }
+}
+@media (max-width: 576px) {
+  .margin-left{
+  margin-left: -1rem;
+  }
 }
 </style>
